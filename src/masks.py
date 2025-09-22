c232f3de8f96f14fc384ada4195586c2aa2aae40
IKolesnikov <76kolesnikof@gmail.com>
2025-09-22 18:52:21 +0300

    add names.txt

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция принимает на вход номер карты и возвращает его маску"""
+
+    number_to_str = str(card_number)
+    mask_number = number_to_str.replace(number_to_str[6:-4], "******")
+    mask_card_number = " ".join(mask_number[i : i + 4] for i in range(0, len(mask_number), 4))
+    return mask_card_number
+
+
+def get_mask_account(account_num: int) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+
+    mask_account = "**" + str(account_num)[-4:]
+    return mask_account
+
+
