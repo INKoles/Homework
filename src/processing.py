26138d127ae8ad107b166872bb302ebe8e4ac3a7
IKolesnikov <76kolesnikof@gmail.com>
2025-10-10 19:59:08 +0300

    correct with linters processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
-def filter_by_state(bank_operation: list[dict], state: str = 'EXECUTED') -> list[dict]:
-    """ Принимает список словарей и опционально значение для ключа
+def filter_by_state(bank_operation: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Принимает список словарей и опционально значение для ключа
     state (по умолчанию 'EXECUTED').
     Возвращает новый список словарей, содержащий только те словари,
-    у которых ключ state соответствует указанному значению. """
+    у которых ключ state соответствует указанному значению."""
 
     filter_tranz = []
     if not bank_operation:
@@ -14,11 +14,9 @@ def filter_by_state(bank_operation: list[dict], state: str = 'EXECUTED') -> list
 
 
 def sort_by_date(bank_operation: list[dict], ascendig: bool = True) -> list[dict]:
-    """ Принимает список словарей и необязательный параметр,
+    """Принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание).
-    Должна возвращать новый список, отсортированный по дате (date). """
-
+    Должна возвращать новый список, отсортированный по дате (date)."""
 
     sorted_bank_operation = sorted(bank_operation, key=lambda x: x["date"], reverse=ascendig)
     return sorted_bank_operation
-
