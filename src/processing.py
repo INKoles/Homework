f0249a2931d6fb376459675d52868904a84865cf
IKolesnikov <76kolesnikof@gmail.com>
2025-10-13 02:21:19 +0300

    In the "processing" file, the default value "sorted" and the variable name have been adjusted. The "readme" file has been supplemented.

--- src/processing.py
+++ src/processing.py
@@ -4,19 +4,19 @@ def filter_by_state(bank_operation: list[dict], state: str = "EXECUTED") -> list
     Возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению."""
 
-    filter_tranz = []
+    transaction_filter = []
     if not bank_operation:
         return []
-    for tranz in bank_operation:
-        if tranz["state"] == state:
-            filter_tranz.append(tranz)
-    return filter_tranz
+    for transaction in bank_operation:
+        if transaction["state"] == state:
+            transaction_filter.append(transaction)
+    return transaction_filter
 
 
-def sort_by_date(bank_operation: list[dict], ascendig: bool = True) -> list[dict]:
+def sort_by_date(bank_operation: list[dict], sort_by_descending: bool = True) -> list[dict]:
     """Принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание).
     Должна возвращать новый список, отсортированный по дате (date)."""
 
-    sorted_bank_operation = sorted(bank_operation, key=lambda x: x["date"], reverse=ascendig)
+    sorted_bank_operation = sorted(bank_operation, key=lambda x: x["date"], reverse=sort_by_descending)
     return sorted_bank_operation
